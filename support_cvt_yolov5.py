f2d74792abda65f7fe2ae872b1f87951fdc1ad06
kimdonggyun <kdk921219@gamil.com>
2021-12-08 17:23:33 +0100

    yolo_2_dataframe done

--- support_cvt_yolov5.py
+++ support_cvt_yolov5.py
@@ -5,7 +5,9 @@ from genericpath import getsize
 import cv2
 import glob
 import os
+import re
 from pathlib import Path
+import pandas as pd
 
 def img_2_video (filepath, video_name, fps):
     """
@@ -48,16 +50,33 @@ def remove_empty_frame(dirpath):
         os.remove(z) # remove lables
         os.remove(z.replace(".txt", ".PNG")) # remove images
 
-def yolo_2_dataframe():
+def yolo_2_dataframe(filepath):
     """
     convert txt file from yolo prediction and convert it to dataframe
     """
+    # read in dataframe
+    df = pd.read_csv(filepath, sep="\),|\)]," , header=None, engine="python")
+    df.columns = ["frame_name", "X1", "Y1", "X2", "Y2", "label"]
+
+    # modify data to get a form of easier use
+    df["frame_name"] = df["frame_name"].apply(lambda x: x.replace("[PosixPath(", ""))
+
+    df["X1"] = df["X1"].apply(lambda x: float(re.findall(r"\d+" , x)[0]) )
+    df["Y1"] = df["Y1"].apply(lambda x: float(re.findall(r"\d+" , x)[0]) )
+    df["X2"] = df["X2"].apply(lambda x: float(re.findall(r"\d+" , x)[0]) )
+    df["Y2"] = df["Y2"].apply(lambda x: float(re.findall(r"\d+" , x)[0]) )
+
+    df["probability"] = df["label"].apply(lambda x: float(re.findall(r"\d+.\d+" , x)[0]) ) # first create probability
+    df["label"] = df["label"].apply(lambda x: re.findall(r"\w+" , x)[0]) # and then label
     
+    # save dataframe
+    df.to_csv(filepath.replace(".txt", "_df.txt"), sep="\t")
+    print("file is saved at %s" %(filepath.replace(".txt", "df.txt") ,))
 
 
 
 if __name__ == "__main__":
-    run = int(input("which function?(0: img_2_video, 1: remove_empty_frame):, 2: yolo_2_dataframe"))
+    run = int(input("which function?(0: img_2_video, 1: remove_empty_frame):, 2: yolo_2_dataframe :"))
     if run == 0:
         filepath = input("type full filepath and *.container_name: ")
         savename = input("type video name (including video container): ")
@@ -67,6 +86,6 @@ if __name__ == "__main__":
         dirpath = input("type full dirpath: ")
         remove_empty_frame(dirpath)
     elif run == 2:
-        filepath = input("type fill filepaht: ")
+        filepath = input("type full filepath: ")
         yolo_2_dataframe(filepath)
 
