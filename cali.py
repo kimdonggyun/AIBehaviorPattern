8c61f681a35600df3dd8d4900d942779b43e1131
kimdonggyun <kdk921219@gamil.com>
2021-12-14 20:01:40 +0100

    definitions

--- cali.py
+++ cali.py
@@ -1,1 +1,16 @@
 # video calibration and modifying for precise analysis
+
+def video_rotation(filepath):
+    # rotate video to get right align
+
+
+def cal_img_param():
+    # calculate image parameteres
+
+
+
+def align_frame():
+    # align the frame as two cameras have slightly different height
+
+
+def 
