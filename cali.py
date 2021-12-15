e081a66837f209d06312ba15841820cd53144cbc
kimdonggyun <kdk921219@gamil.com>
2021-12-15 17:36:47 +0100

    calculating real pixel size

--- cali.py
+++ cali.py
@@ -1,4 +1,5 @@
 # video calibration and modifying for precise analysis
+import numpy as np
 
 def video_rotation(filepath):
     # rotate video to get right align
@@ -13,4 +14,15 @@ def align_frame():
     # align the frame as two cameras have slightly different height
 
 
-def 
+def obj_size(pixel_area, constant):
+    """
+    calculte the real size of object depending on it's distance to lens
+    parameters to consider:
+    pixel_area = calculated pixel area of an object
+    distance = distance between object and lens
+    constant = known constanct value (img_size_A * object_A_distance == img_size_B * object_B_distance)
+    """
+    
+    constant = np.square(pixel_area)*distance
+    real_size = pixel_area
+    return real_size
